ea39294e6b272512331da4f0a2b194f778c28c2a
cynthia-qin <1795805647@qq.com>
2022-07-07 21:27:33 +0800

    完成树形组件封装

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px; width: 100%"
+  >
+    <!-- 左边列 -->
+    <el-col
+      ><span>{{ treeNode.name }}</span></el-col
+    >
+    <!-- 右边列 -->
+    <el-col :span="4">
+      <!-- 控制内容flex -->
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 -->
+          <el-dropdown>
+            <span>操作<i class="el-icon-arrow-down"></i></span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除子部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    // 传入一个对象，根据对象里的内容渲染数据
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    // 判断当前使用插槽的是否是首行 ，首行就只有一个操作
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () { },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 卡片 -->
+      <el-card class="tree-card">
+        <!-- 首行 -->
+        <tree-tools :tree-node="company" :is-root="true" />
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+        <!-- 因为el-tree会根据data接收的数组的长度循环内部tree-tools，没循环一次就会有一个新的node和data产生 表示的是当前循环项的数据 我们可以根据这个渲染数据 -->
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
-
+  components: {
+    treeTools
+  },
+  data () {
+    return {
+      departs: [
+        { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        label: 'name'
+      },
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }
+    }
+  }
 }
 </script>
 
 <style>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
